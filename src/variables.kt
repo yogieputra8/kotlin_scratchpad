23d2b12aa01794901760f38752a706076cf3e11c
yogie.putra8 <yogie.putra8@gmail.com>
2018-02-19 00:08:04 +0700

    Add varibales

---
+++ src/variables.kt
@@ -0,0 +1,27 @@
+fun main(args: Array<String>) {
+    val name: String = "Yogie"
+
+    var isAwesome = true
+    isAwesome = false
+
+    println("Is " + name + " awesome? The answer is : " + isAwesome)
+    println("$name is awesome, yes it $isAwesome")
+
+
+    var a: Int = 3
+    var b = 6
+    println(a + b)
+
+    val doubleNum: Double = 123.45 // 64 bit number
+    val floatNum: Float = 123.45f // 32 bit number
+
+    val longNum: Long = 123651371352313213L // 64 bit
+    val aDouble = a.toString()
+
+    var hero: String
+    hero = "batman"
+    hero = "superman"
+    println(hero)
+
+
+}
