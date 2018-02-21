f5a5f1d70fc6c36a21983cbd4b55515f0e8b33d8
yogie.putra8 <yogie.putra8@gmail.com>
2018-02-21 22:57:29 +0700

    Add functions

---
+++ src/Functions.kt
@@ -0,0 +1,27 @@
+fun main(args: Array<String>) {
+
+    fun forceChoke(){
+        println("Hello, my name is yogie...")
+    }
+    forceChoke()
+
+    fun greetings(name: String){
+        println("Hello, my name is $name")
+    }
+    greetings("yogie")
+
+    fun calculateNumbers(angka1: Int, angka2: Int): Int {
+        val result = angka1 + angka2
+        return result
+    }
+    val result = calculateNumbers(3, 8)
+    println(result)
+    println("Hi, ${calculateNumbers(10, 6)}")
+
+    fun feeling(mood: String = "angry"){
+        println(mood)
+    }
+    feeling("meh")
+    
+
+}
